a68f75eb21d91bd7b10861134fdb1b36f1a2efc6
luisgomezm78 <luis_gomezm@hotmail.com>
2023-10-29 11:15:33 -0500

    Registro Aportaciones

---
+++ src/pages/RegistroApo/index.jsx
@@ -0,0 +1,12 @@
+import { Frame, FormApo } from "../../components";
+
+export default function RegistroApo () {
+    return (
+        <>
+        <Frame className="w-1/3">
+          <FormApo />
+        </Frame>
+      </>
+    );
+
+}
