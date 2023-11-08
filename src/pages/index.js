5f0fec3dff4d2ce54b393606315b7c3ec4a0f529
IvanejoPerelo <ivan.escorpio1@gmail.com>
2023-11-08 12:20:28 -0500

    salvando

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,12 @@
 
 export { default as Home } from "./Home";
-
-export {default as ListarEmp} from "./ListarEmp";
 export { default as Login } from "./Login";
-
 export { default as ModifAfp } from "./ModifAfp";
 export { default as ModifApo } from "./ModifApo";
 export { default as ModifDes } from "./ModifDes";
 export { default as ModifEmp } from "./ModifEmp";
 export { default as ModifHab } from "./ModifHab";
-
-
 export {default as Planillas} from "./Planillas";
-
 export { default as RegistroAfp } from "./RegistroAfp";
 export { default as RegistroApo } from "./RegistroApo";
 export { default as RegistroDes } from "./RegistroDes";
